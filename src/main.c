44a3e843b95f28e667b5cfad6aea4dbf51e1642d
melinda <melinda.stobbe@mail.de>
2026-03-15 10:13:05 +0100

    Did some shit WIP Ill put an actual commit message when i finish this

--- src/main.c
+++ src/main.c
@@ -3,12 +3,16 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 #include <SDL3_ttf/SDL_ttf.h>
+#include <math.h>
 #include <stdio.h>
 
 #define DEF_SCREENWIDTH 1280
 #define DEF_SCREENHEIGHT 720
 #define DEF_FPS 60
 #define DEF_FPS_TEXTBUFFER_SIZE 64
+
+#define DEG2RAD 0.01745329252
+#define RAD2DEG 57.29577951
 
 #define SDL_Err(fmt, ...)                                                      \
 	do {                                                                       \
@@ -18,15 +22,36 @@
 		);                                                                     \
 	} while(0)
 
-typedef struct {
-	float x;
-	float y;
-} PointF;
+#define REG_TEXTURE(tex_embed_var)                                             \
+	{NULL, tex_embed_var, sizeof(tex_embed_var), {0.0f, 0.0f, 0.0f, 0.0f}}
+
+enum Texture_Cache_Textures { TEXTURE_PLAYER, TEXTURES_NUM };
+enum Keys {
+
+};
 
 typedef struct {
-	bool   alive;
-	PointF pos;
-	float  move_speed;
+	SDL_Texture*   tex;
+	const uint8_t* tex_data;
+	size_t         tex_size;
+	SDL_FRect      src;
+} Texture;
+
+const uint8_t EMBED_TEXTURE_PLAYER[] = {
+#embed "../res/player.png"
+};
+
+Texture textures[TEXTURES_NUM] = {
+	REG_TEXTURE(EMBED_TEXTURE_PLAYER),
+};
+
+typedef struct {
+	bool         alive;
+	SDL_FPoint   pos;
+	SDL_FPoint   vel;
+	double       rot;
+	float        move_speed;
+	SDL_Texture* tex;
 } Player;
 
 typedef struct {
@@ -39,6 +64,7 @@ typedef struct {
 	TTF_Font*       font;
 	TTF_TextEngine* text_engine;
 	TTF_Text*       hewo;
+	uint8_t
 } Engine;
 
 SDL_Point screensize = {DEF_SCREENWIDTH, DEF_SCREENHEIGHT};
@@ -51,6 +77,16 @@ const char iosevka_font[] = {
 
 static SDL_Window*   window   = NULL;
 static SDL_Renderer* renderer = NULL;
+
+SDL_FPoint pointf_add(const SDL_FPoint a, const SDL_FPoint b) {
+	return (SDL_FPoint) {a.x + b.x, a.y + b.y};
+}
+
+SDL_FPoint pointf_rotate(const SDL_FPoint a, const float deg) {
+	const float x = (a.x * cos(deg * DEG2RAD)) - (a.y * sin(deg * DEG2RAD));
+	const float y = (a.x * sin(deg * DEG2RAD)) + (a.y * cos(deg * DEG2RAD));
+	return (SDL_FPoint) {x, y};
+}
 
 void engine_update_frame(void) {
 	// Get current fps
@@ -71,7 +107,8 @@ void engine_update_frame(void) {
 }
 
 void engine_draw_frame(void) {
-	SDL_FRect player_rect = {player.pos.x, player.pos.y, 50, 50};
+	SDL_FRect  player_rect = {player.pos.x, player.pos.y, 50, 50};
+	SDL_FPoint player_ctr  = {player.pos.x + 25, player.pos.y + 25};
 
 	// Grey background
 	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
@@ -79,7 +116,10 @@ void engine_draw_frame(void) {
 
 	// Draw player
 	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-	SDL_RenderFillRect(renderer, &player_rect);
+	SDL_RenderTextureRotated(
+		renderer, textures[TEXTURE_PLAYER].tex, &textures[TEXTURE_PLAYER].src,
+		&player_rect, player.rot, &player_ctr, SDL_FLIP_NONE
+	);
 
 	// Draw FPS
 	SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
@@ -105,6 +145,9 @@ SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[]) {
 		SDL_Err("Failed to initizalized window/renderer");
 		return SDL_APP_FAILURE;
 	}
+
+	// Disable AA
+	SDL_SetDefaultTextureScaleMode(renderer, SDL_SCALEMODE_PIXELART);
 
 	// Try setup font lib
 	if(!TTF_Init()) {
@@ -134,9 +177,30 @@ SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[]) {
 		return SDL_APP_FAILURE;
 	}
 
+	// Try setup textures
+	for(uint32_t i = 0; i < TEXTURES_NUM; i++) {
+		SDL_Surface* surface = SDL_LoadPNG_IO(
+			SDL_IOFromConstMem(textures[i].tex_data, textures[i].tex_size), true
+		);
+		if(!surface) {
+			SDL_Err("Failed to load texture %d into RAM", i);
+			return SDL_APP_FAILURE;
+		}
+		textures[i].tex = SDL_CreateTextureFromSurface(renderer, surface);
+		if(!textures[i].tex) {
+			SDL_Err("Failed to load texture %d into VRAM", i);
+			SDL_DestroySurface(surface);
+			return SDL_APP_FAILURE;
+		}
+		textures[i].src = (SDL_FRect) {0.0f, 0.0f, surface->w, surface->h};
+		SDL_DestroySurface(surface);
+	}
+
 	// Arbitrary initializations
 	player.alive      = true;
-	player.pos        = (PointF) {screensize.x >> 1, screensize.y >> 1};
+	player.pos        = (SDL_FPoint) {screensize.x >> 1, screensize.y >> 1};
+	player.vel        = (SDL_FPoint) {0.0, 0.0};
+	player.rot        = 0.0;
 	player.move_speed = 50;
 
 	engine.last_frame  = 0;
